628c20a0fb3b816d58e96e6929a4e321fa6af8a8
silverowiec <silverowiec@gmail.com>
2026-03-26 02:28:35 +0100

    fix: update POOL_INIT_CODE_HASH for Shanghai EVM bytecode
    The hardcoded hash was from Solidity 0.7.6 compilation. After porting
    v3-core to 0.8.x and compiling with evm_version=shanghai, the
    UniswapV3Pool bytecode changed, invalidating the CREATE2 address
    computation. SwapRouter was resolving wrong pool addresses, causing
    all V3 swaps to revert.
    
    New hash computed from: keccak256(type(UniswapV3Pool).creationCode)
    with solc 0.8.26, evm_version=shanghai, via_ir=false.

--- contracts/libraries/PoolAddress.sol
+++ contracts/libraries/PoolAddress.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0;
 
 /// @title Provides functions for deriving a pool address from the factory, tokens, and the fee
 library PoolAddress {
-    bytes32 internal constant POOL_INIT_CODE_HASH = 0xa598dd2fba360510c5a8f02f44423a4468e902df5857dbce3ca162a43a3a31ff;
+    bytes32 internal constant POOL_INIT_CODE_HASH = 0x2076fc70544b770eebed12b5bc8c1a1c9c3f10f8a57778e553288520cfb8d4d8;
 
     /// @notice The identifying key of the pool
     struct PoolKey {
